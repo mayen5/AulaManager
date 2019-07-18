446b1a7fb66249030d1e02dff81b11b5e7061ace
Carmelo Mayen <carmelo5mayen@gmail.com>
2019-07-18 07:32:13 -0600

    Add project files.

---
+++ AulaManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AulaManager.Startup))]
+namespace AulaManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
